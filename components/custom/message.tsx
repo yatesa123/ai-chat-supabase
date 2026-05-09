eae9f2c9378fdff5168bc78d5347521c8f68bfac
yatesa123 <learningpod123@gmail.com>
2026-05-09 00:50:57 -0400

    Replace FaRegSparkles with LuSparkles icon

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -10,7 +10,7 @@ import { Vote } from '@/lib/supabase/types';
 import { UIBlock } from './block';
 import { DocumentToolCall, DocumentToolResult } from './document';
 //@ts-ignore
-import { FaRegSparkles } from 'react-icons/fa';
+import { LuSparkles } from 'react-icons/lu';
 import { Markdown } from './markdown';
 import { MessageActions } from './message-actions';
 import { PreviewAttachment } from './preview-attachment';
