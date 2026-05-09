d100bf82b13a1a11084380d7d0d9fc6c36a166dd
yatesa123 <learningpod123@gmail.com>
2026-05-09 01:15:55 -0400

    Remove FaRegSparkles icon from assistant message

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -44,7 +44,6 @@ export const PreviewMessage = ({
       >
         {message.role === 'assistant' && (
           <div className="size-8 flex items-center rounded-full justify-center ring-1 shrink-0 ring-border">
-            <FaRegSparkles size={14} />
           </div>
         )}
 
